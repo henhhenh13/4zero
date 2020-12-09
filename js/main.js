870586903739fa70a0142dec0312518aa5a2f6b6
henhhenh13 <74145131+henhhenh13@users.noreply.github.com>
2020-12-09 09:11:59 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,7 +19,6 @@ function onHead(){
       nav.classList.remove('stuck')
   });
 };
-
 onHead();
 
 
@@ -27,9 +26,9 @@ onHead();
 
 
 
-  const body = document.querySelector('body');
-  const closeX = document.querySelector('.close-full');
-  const searchHead = document.querySelector('.search-s');
+
+// Bật and tắt form search
+const searchHead = document.querySelector('.search-s');
   const searchChild = document.querySelector('.search-child');
   const fillOne = document.querySelector('.fill1');
   const fillTwo = document.querySelector('.fill2');
@@ -37,8 +36,6 @@ onHead();
   const userOn = document.querySelector('.user-on');
   const formLogin = document.querySelector('.form-login');
   const formDown = document.querySelector('.form');
-
-// Bật and tắt form search
 function ghimSearch(){
   searchHead.classList.toggle('active');
   body.classList.toggle('ghim');
@@ -75,60 +72,91 @@ fillOne.addEventListener('click',function(){
 
 
 
-// Show menu header of phone
-const menu = document.querySelector('#id-head-nav');
-const freeDom = document.querySelector('.free-dom');
-const fullMenu = document.querySelector('.full-menu');
-function showMenuPhone(){
-  menu.classList.toggle('show-nav');
-  freeDom.classList.toggle('on'); 
-  fullMenu.classList.toggle('on'); 
-  closeX.classList.toggle('chay'); 
-  body.classList.toggle('ghim');
-}
 
-// Tắt hết sub-menu đã active
-function closeHet(){
-  menu.classList.remove('show-nav');
-  freeDom.classList.remove('on'); 
-  fullMenu.classList.remove('on'); 
-  closeX.classList.remove('chay'); 
-  body.classList.remove('ghim');
+// Thứ viện menudropdown by: henhpropd
+function menuDropDownMobile(options) {
+  var menuShowMobile = document.querySelector(options.menuShowMobile);
+  const body = document.querySelector('body');
+  if (menuShowMobile) {
+    var btnShowMenus =  document.querySelector(options.btnShowMenus);
+    var closeAll = document.querySelectorAll(options.closeMenus);
+    // Xử lý sự kiện Đóng mở Menus
+    if (btnShowMenus) {
+      // Mở Menus
+        btnShowMenus.onclick = function() {
+        menuShowMobile.classList.add('show-nav');
+        body.classList.add('ghim')
+        closeAll.forEach(function(close){
+          close.classList.add('on')
+        })
 
-  for (let index = 1; index <= 4; index++) {
-    const closeFull = document.querySelector('.sub-'+index);
-    closeFull.classList.remove('show')
+      }
+      // Xử lý sợ kiện mở đóng menu Dropdown
+      var btnSub = menuShowMobile.querySelectorAll(options.btnShowMenuSub);
+      var subMenuAll = menuShowMobile.querySelectorAll(options.subMenu);
+      if (btnSub) {
+        btnSub.forEach(function (btn){
+          var subMenus = btn.parentElement.querySelector(options.subMenu);
+          btn.onclick = function() {
+            btn.classList.toggle('quay');
+            subMenus.classList.toggle('show');
+          }
+        });
+      }
+
+      // Đóng Menus + dropdown-menu
+      if(closeAll) {
+        for (let i = 0; i < closeAll.length; i++) {
+          // Tắt ghim body and đóng menu
+          closeAll[i].onclick = function() {
+            menuShowMobile.classList.remove('show-nav');
+            body.classList.remove('ghim');
+            // Đóng menu-sub
+            for (let i = 0; i < btnSub.length; i++) {
+              btnSub[i].classList.remove('quay');
+              subMenuAll[i].classList.remove('show');
+            }
+            // Tắt nút X và lớp fill X;
+            for (let i = 0; i < closeAll.length; i++) {
+              closeAll[i].classList.remove('on');
+            }
+          }
+        }
+      }
+    };
   }
 }
 
-// Quay -180 of sub-menu
-function changeSub1(){
-  var pro = document.querySelector('.sub-1');
-  pro.classList.toggle('show');
 
-  var btn_sub = document.querySelector('.btn-sub-1');
-  btn_sub.classList.toggle('quay');
-}
-function changeSub2(){
-  var pro = document.querySelector('.sub-2');
-  pro.classList.toggle('show');
 
-  var btn_sub = document.querySelector('.btn-sub-2');
-  btn_sub.classList.toggle('quay');
-}
-function changeSub3(){
-  var pro = document.querySelector('.sub-3');
-  pro.classList.toggle('show');
 
-  var btn_sub = document.querySelector('.btn-sub-3');
-  btn_sub.classList.toggle('quay');
-}
-function changeSub4(){
-  var pro = document.querySelector('.sub-4');
-  pro.classList.toggle('show');
 
-  var btn_sub = document.querySelector('.btn-sub-4');
-  btn_sub.classList.toggle('quay');
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
